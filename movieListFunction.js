303b3d8c82a0bb26db880cf75190c0add59d3e69
Preeti <preemishra99@gmail.com>
2022-11-24 15:27:08 +0530

    Adding some changes

---
+++ movieListFunction.js
@@ -0,0 +1,34 @@
+movieList([
+    {
+       "title": "ABCD",
+       "url": "https://www.youtube.com/watch?v=HCeBd5GKNO8"
+    },
+    {
+        "title": "ROBOT",
+        "url": "https://www.youtube.com/watch?v=HCeBd5GKNO8"
+    },
+    {
+        "title": "Gully Boy",
+        "url": "https://www.youtube.com/watch?v=HCeBd5GKNO8"
+     },
+     {
+         "title": "Kantara",
+         "url": "https://www.youtube.com/watch?v=HCeBd5GKNO8"
+     },
+     {
+        "title": "Black Panther: Wakanda Forever",
+        "url": "https://www.youtube.com/watch?v=HCeBd5GKNO8"
+     },
+     {
+         "title": "The Wonder",
+         "url": "https://www.youtube.com/watch?v=HCeBd5GKNO8"
+     },
+     {
+         "title": "KGF",
+         "url": "https://www.youtube.com/watch?v=HCeBd5GKNO8"
+      },
+      {
+          "title": "Sooryavansham",
+          "url": "https://www.youtube.com/watch?v=HCeBd5GKNO8"
+      }
+])
